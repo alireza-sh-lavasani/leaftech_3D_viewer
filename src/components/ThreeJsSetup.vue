a05eeae37381013a662f36d90c4d8bad360e6f8f
Alireza Lavasani <alireza.sh.lavasani@gmail.com>
2022-05-23 23:32:34 +0430

    added building

--- src/components/ThreeJsSetup.vue
+++ src/components/ThreeJsSetup.vue
@@ -10,8 +10,11 @@ import {
   Color,
   SpotLight,
   AxesHelper,
+  Clock,
 } from 'three'
 import { onMounted, ref } from 'vue'
+import gsap from 'gsap'
+import STLLoader from './loaders/STLLoader'
 
 // Ref
 const viewer = ref()
@@ -22,19 +25,18 @@ scene.background = new Color('#444d5c')
 
 // Camera
 const camera = new PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 )
-camera.position.x = 1
-camera.position.y = 2
-camera.position.z = 5
-camera.rotation.x = -0.5
+camera.position.set(1, 1, 9)
+
+// camera.rotation.x = -0.5
 
 // Light
 const light = new SpotLight('hsl(0, 100%, 100%)')
-light.position.set(2, 3, 2)
+light.position.set(10, 5, 5)
 scene.add(light)
 
 // Renderer
@@ -45,30 +47,52 @@ renderer.setSize(1280, 720)
 const geometry = new BoxGeometry()
 const material = new MeshStandardMaterial({
   side: FrontSide,
-  color: 'hsl(0, 100%, 50%)',
+  color: 'hsl(0, 0, 50%)',
   wireframe: false,
 })
 const cube = new Mesh(geometry, material)
-scene.add(cube)
+// scene.add(cube)
+
+const loader = new STLLoader()
+
+loader.load('assets/STL_files/test_building.stl', geo => {
+  const building = new Mesh(geo, material)
+  // building.rotation.y = Math.PI * -0.5
+
+  scene.add(building)
+
+  camera.lookAt(building.position)
+})
 
 // Axes
-const axes = new AxesHelper(5)
+const axes = new AxesHelper(50)
 scene.add(axes)
 
-// Animate
-const animate = () => {
-  requestAnimationFrame(animate)
+const clock = new Clock()
 
-  cube.rotation.y += 0.01
+// Animatations
+const tick = () => {
+  // Time
+  const elapsedTime = clock.getElapsedTime()
+
+  // Object transformations
+  // camera.position.y = Math.sin(elapsedTime * 1)
+  // camera.position.x = Math.cos(elapsedTime * 1)
+  // camera.lookAt(cube.position)
 
   // Render
   renderer.render(scene, camera)
+
+  requestAnimationFrame(tick)
 }
 
 onMounted(() => {
   viewer.value.appendChild(renderer.domElement)
-  animate()
+  tick()
 })
+
+// gsap.to(cube.position, { x: 2, duration: 2 })
+// gsap.to(cube.position, { x: 0, duration: 2, delay: 2 })
 </script>
 
 <template>
