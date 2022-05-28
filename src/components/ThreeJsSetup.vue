718bb82c0506a68a1db09be770dd735da48d339d
Alireza Lavasani <alireza.sh.lavasani@gmail.com>
2022-05-28 16:48:02 +0430

    added debug UI, removed VUE reactivity

--- src/components/ThreeJsSetup.vue
+++ src/components/ThreeJsSetup.vue
@@ -13,104 +13,140 @@ import {
   Clock,
   Vector3,
 } from 'three'
-import { onMounted, ref } from 'vue'
-import gsap from 'gsap'
+import { onMounted, ref, watchEffect } from 'vue'
+// import gsap from 'gsap'
 import STLLoader from './loaders/STLLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GUI } from 'dat.gui'
 
-// Ref
-const viewer = ref()
-const cursor = ref({ x: 0, y: 0 })
-
-// Scene
-const scene = new Scene()
-scene.background = new Color('#444d5c')
-
-// Camera
-const camera = new PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-)
-camera.position.set(3, 3, 9)
-
-// Light
-const light = new SpotLight('hsl(0, 100%, 100%)')
-light.position.set(10, 5, 5)
-scene.add(light)
-
-// Renderer
-const viewportSize = {
-  x: 1280,
-  y: 720,
-}
-const renderer = new WebGLRenderer()
-renderer.setSize(viewportSize.x, viewportSize.y)
-
-// Mesh
-const geometry = new BoxGeometry()
-const material = new MeshStandardMaterial({
-  side: FrontSide,
-  color: 'hsl(0, 0, 50%)',
-  wireframe: false,
-})
-const cube = new Mesh(geometry, material)
-// scene.add(cube)
-
-// Building
+/**************************************
+ ******** STL Load
+ *************************************/
 const loader = new STLLoader()
 loader.load('assets/STL_files/test_building.stl', geo => {
-  const building = new Mesh(geo, material)
-  scene.add(building)
-})
+  /**************************************
+   ******** GUI
+   *************************************/
+  const gui = new GUI()
 
-// Axes
-const axes = new AxesHelper(50)
-scene.add(axes)
+  /**************************************
+   ******** Scene
+   *************************************/
+  const scene = new Scene()
+  scene.background = new Color('#444d5c')
 
-const clock = new Clock()
+  /**************************************
+   ******** Camera
+   *************************************/
+  const camera = new PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+  camera.position.set(3, 3, 9)
 
-// Animatations
-const tick = () => {
-  // Time
-  const elapsedTime = clock.getElapsedTime()
+  /**************************************
+   ******** Light
+   *************************************/
+  const light = new SpotLight('hsl(0, 100%, 100%)')
+  light.position.set(10, 5, 5)
+  scene.add(light)
 
-  // Object transformations
-  // camera.position.y = Math.sin(elapsedTime * 1)
-  // camera.position.x = Math.cos(elapsedTime * 1)
-  // camera.lookAt(cube.position)
+  /**************************************
+   ******** Renderer
+   *************************************/
+  const viewportSize = {
+    x: 1280,
+    y: 720,
+  }
+  const renderer = new WebGLRenderer()
+  renderer.setSize(viewportSize.x, viewportSize.y)
+  document.querySelector('#main').appendChild(renderer.domElement)
 
-  // camera.position.x = Math.sin(cursor.value.x * -2 * Math.PI) * 10
-  // camera.position.z = Math.cos(cursor.value.x * -2 * Math.PI) * 10
-  // camera.position.y = cursor.value.y * 10
-  // camera.lookAt(new Vector3(0))
-
-  // camera.position.x = cursor.x
-  // camera.position.y = cursor.y
-
-  // Render
-  renderer.render(scene, camera)
-
-  requestAnimationFrame(tick)
-}
-
-onMounted(() => {
-  viewer.value.appendChild(renderer.domElement)
-
+  /**************************************
+   ******** Controls
+   *************************************/
   const controls = new OrbitControls(camera, renderer.domElement)
+  // controls.autoRotate = true
+  controls.enableDamping = true
+  controls.dampingFactor = 0.1
+
+  /**************************************
+   ******** Mesh
+   *************************************/
+  const geometry = new BoxGeometry()
+  const material = new MeshStandardMaterial({
+    side: FrontSide,
+    color: 'hsl(0, 0, 50%)',
+    wireframe: false,
+  })
+  const cube = new Mesh(geometry, material)
+  // scene.add(cube)
+
+  /**************************************
+   ******** Building
+   *************************************/
+  const building = new Mesh(geo, material)
+  gui
+    .add(building.position, 'x')
+    .name('Test Position')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+
+  scene.add(building)
+
+  /**************************************
+   ******** Axes
+   *************************************/
+  const axes = new AxesHelper(50)
+  scene.add(axes)
+
+  const clock = new Clock()
+
+  /**************************************
+   ******** Animatations
+   *************************************/
+  // gsap.to(cube.position, { x: 2, duration: 2 })
+  // gsap.to(cube.position, { x: 0, duration: 2, delay: 2 })
+
+  const tick = () => {
+    // Time
+    const elapsedTime = clock.getElapsedTime()
+
+    // Object transformations
+    // camera.position.y = Math.sin(elapsedTime * 1)
+    // camera.position.x = Math.cos(elapsedTime * 1)
+    // camera.lookAt(cube.position)
+
+    // camera.position.x = Math.sin(cursor.value.x * -2 * Math.PI) * 10
+    // camera.position.z = Math.cos(cursor.value.x * -2 * Math.PI) * 10
+    // camera.position.y = cursor.value.y * 10
+    // camera.lookAt(new Vector3(0))
+
+    // camera.position.x = cursor.x
+    // camera.position.y = cursor.y
+
+    // required if controls.enableDamping or controls.autoRotate are set to true
+    controls.update()
+
+    /**************************************
+     ******** Render
+     *************************************/
+    renderer.render(scene, camera)
+
+    requestAnimationFrame(tick)
+  }
 
   tick()
 })
-
-// gsap.to(cube.position, { x: 2, duration: 2 })
-// gsap.to(cube.position, { x: 0, duration: 2, delay: 2 })
 </script>
 
 <template>
   <h1>3D Viewer</h1>
 
-  <main ref="viewer"></main>
+  <main id="main"></main>
 </template>
 
 <style></style>
